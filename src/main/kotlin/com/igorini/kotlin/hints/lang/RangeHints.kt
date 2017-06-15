4c9e1a6628f94087a7208de94e75a57fbee0f33d
Igor Maguskin <maguskin.igor@gmail.com>
2017-06-15 23:09:34 +0100

    Removed redundant fully qualified part of a reference to a constant in `RangeHints.kt`

--- src/main/kotlin/com/igorini/kotlin/hints/lang/RangeHints.kt
+++ src/main/kotlin/com/igorini/kotlin/hints/lang/RangeHints.kt
@@ -6,10 +6,10 @@ const val LIMIT = 10
 
 // [FROM]
 fun fromUpperLimit(){
-    for (i in 1..(com.igorini.kotlin.hints.lang.LIMIT - 1)) println(i)
+    for (i in 1..(LIMIT - 1)) println(i)
 }
 
 // [TO]
 fun toUpperLimit() {
-    for (i in 1 until com.igorini.kotlin.hints.lang.LIMIT) println(i)
+    for (i in 1 until LIMIT) println(i)
 }
