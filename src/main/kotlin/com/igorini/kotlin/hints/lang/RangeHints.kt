632e1adcbce386f82408408a2bf49b3faa8780ce
Igor Maguskin <maguskin.igor@gmail.com>
2017-06-15 23:24:38 +0100

    Improved the hint message in `RangeHints.kt`. Added comments.

--- src/main/kotlin/com/igorini/kotlin/hints/lang/RangeHints.kt
+++ src/main/kotlin/com/igorini/kotlin/hints/lang/RangeHints.kt
@@ -1,6 +1,8 @@
 package com.igorini.kotlin.hints.lang
 
-// [Exclusive upper limit] -> Use `until` operator
+// [Exclusive upper limit] -> Use `until` operator to exclude the upper limit.
+
+// Printing out numbers from 1 up to but excluding 10
 
 const val LIMIT = 10
 
