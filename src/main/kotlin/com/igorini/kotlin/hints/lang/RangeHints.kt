469fa49a85079d9463b5f99daf1bea4ce061b9b1
Igor Maguskin <maguskin.igor@gmail.com>
2017-06-15 23:07:05 +0100

    Made property a constant in `RangeHints.kt`

--- src/main/kotlin/com/igorini/kotlin/hints/lang/RangeHints.kt
+++ src/main/kotlin/com/igorini/kotlin/hints/lang/RangeHints.kt
@@ -2,14 +2,14 @@ package com.igorini.kotlin.hints.lang
 
 // [Iteration up to an exclusive upper limit] -> Use `until` operator
 
-val limit = 10
+const val LIMIT = 10
 
 // [FROM]
 fun fromUpperLimit(){
-    for (i in 1..(com.igorini.kotlin.hints.lang.limit - 1)) println(i)
+    for (i in 1..(com.igorini.kotlin.hints.lang.LIMIT - 1)) println(i)
 }
 
 // [TO]
 fun toUpperLimit() {
-    for (i in 1 until com.igorini.kotlin.hints.lang.limit) println(i)
+    for (i in 1 until com.igorini.kotlin.hints.lang.LIMIT) println(i)
 }
