aa9bfe4adc06be505dcec50b9dd3dc3cbc12bdd6
Igor Maguskin <maguskin.igor@gmail.com>
2017-06-16 17:44:05 +0100

    Renamed functions and refactored print statements in a `MethodOverload` class

--- src/main/kotlin/com/igorini/kotlin/hints/functions/MethodOverload.kt
+++ src/main/kotlin/com/igorini/kotlin/hints/functions/MethodOverload.kt
@@ -1,12 +1,12 @@
 package com.igorini.kotlin.hints.functions
 
-// [Method Overload] -> Method overloads might be avoided by providing default values for parameters
+// [Method Overload] -> Method overloads can be avoided by providing default values for parameters
 
 // [FROM]
-fun fromFoo() = com.igorini.kotlin.hints.functions.fromFoo(0, "")
-fun fromFoo(a: Int) = com.igorini.kotlin.hints.functions.fromFoo(a, "")
-fun fromFoo(b: String) = com.igorini.kotlin.hints.functions.fromFoo(0, b)
-fun fromFoo(a: Int, b: String) = println("a: $a. b: $b")
+fun fromPrintNumberAndText() = fromPrintNumberAndText(0, "")
+fun fromPrintNumberAndText(a: Int) = fromPrintNumberAndText(a, "")
+fun fromPrintNumberAndText(b: String) = fromPrintNumberAndText(0, b)
+fun fromPrintNumberAndText(a: Int, b: String) = println("Number: $a. Text: $b")
 
 // [TO]
-fun toFoo(a: Int = 0, b: String = "") = println("a: $a. b: $b")
+fun toPrintNumberAndText(a: Int = 0, b: String = "") = println("Number: $a. Text: $b")
