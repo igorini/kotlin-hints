4f7b34d6d73157751266eb6b4a1a1dd9424bb97c
Igor Maguskin <maguskin.igor@gmail.com>
2017-06-17 13:41:36 +0100

    Simplified `MethodOverload` class

--- src/main/kotlin/com/igorini/kotlin/hints/functions/MethodOverload.kt
+++ src/main/kotlin/com/igorini/kotlin/hints/functions/MethodOverload.kt
@@ -3,10 +3,8 @@ package com.igorini.kotlin.hints.functions
 // [Method Overload] -> Method overloads can be avoided by providing default values for parameters
 
 // [FROM]
-fun fromPrintNumberAndText() = fromPrintNumberAndText(0, "")
-fun fromPrintNumberAndText(a: Int) = fromPrintNumberAndText(a, "")
-fun fromPrintNumberAndText(b: String) = fromPrintNumberAndText(0, b)
-fun fromPrintNumberAndText(a: Int, b: String) = println("Number: $a. Text: $b")
+fun fromPrintNumber() = fromPrintNumber(0)
+fun fromPrintNumber(a: Int) = println("Number: $a")
 
 // [TO]
-fun toPrintNumberAndText(a: Int = 0, b: String = "") = println("Number: $a. Text: $b")
+fun toPrintNumber(a: Int = 0) = println("Number: $a")
