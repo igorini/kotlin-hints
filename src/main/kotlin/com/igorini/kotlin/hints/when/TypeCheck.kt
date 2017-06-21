1dbd70f2ecf648dcd842201b32d6683354e5d708
Igor Maguskin <maguskin.igor@gmail.com>
2017-06-21 21:54:34 +0100

    Refactored the class `IsExpression` and renamed it to `TypeCheck`

---
+++ src/main/kotlin/com/igorini/kotlin/hints/when/TypeCheck.kt
@@ -0,0 +1,21 @@
+package com.igorini.kotlin.hints.`when`
+
+// [Type check] -> Type checks can be used in the `when` expression
+
+// [Before]
+fun beforePrintWithType(obj: Any) {
+    if (obj is String) {
+        println("Text: $obj")
+    } else if (obj is Number) {
+        println("Number: $obj")
+    } else {
+        println("Unknown: $obj")
+    }
+}
+
+// [After]
+fun afterPrintWithType(obj: Any) = when (obj) {
+    is String -> println("Text: $obj")
+    is Number -> println("Number: $obj")
+    else -> println("Unknown: $obj")
+}
