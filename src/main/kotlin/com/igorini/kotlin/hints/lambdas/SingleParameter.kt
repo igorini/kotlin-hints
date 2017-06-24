1e02023b35cd6ad1f6b86318be61decc883b02e7
Igor Maguskin <maguskin.igor@gmail.com>
2017-06-24 22:00:10 +0100

    Added `SingleParameter.kt`

---
+++ src/main/kotlin/com/igorini/kotlin/hints/lambdas/SingleParameter.kt
@@ -0,0 +1,12 @@
+package com.igorini.kotlin.hints.lambdas
+
+/* [Single parameter] -> Could use an implicit name `it` to simplify an expression.
+ *
+ * NB: Intellij IDEA has an intention for this case.
+ */
+
+// [Before]
+fun beforePositive(numbers: List<Int>) = numbers.filter { n -> n > 0 }
+
+// [After]
+fun afterPositive(numbers: List<Int>) = numbers.filter { it > 0 }
