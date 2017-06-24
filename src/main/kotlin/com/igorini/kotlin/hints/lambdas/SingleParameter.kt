5136e2636bd0e56dee9a46daf239efa62abe228c
Igor Maguskin <maguskin.igor@gmail.com>
2017-06-24 22:05:09 +0100

    Updated the description in `SingleParameter.kt`

--- src/main/kotlin/com/igorini/kotlin/hints/lambdas/SingleParameter.kt
+++ src/main/kotlin/com/igorini/kotlin/hints/lambdas/SingleParameter.kt
@@ -2,7 +2,7 @@ package com.igorini.kotlin.hints.lambdas
 
 /* [Single parameter] -> Could use an implicit name `it` to simplify an expression.
  *
- * NB: Intellij IDEA has an intention for this case.
+ * NB: Intellij IDEA has an intention that performs the following transformation.
  */
 
 // [Before]
