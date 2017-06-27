7aea80d34c01d4bdc5a9401357c5d74e3a9fe6b8
Igor Maguskin <maguskin.igor@gmail.com>
2017-06-27 12:34:32 +0100

    Added `SafeAccessExpression.kt`

---
+++ src/main/kotlin/com/igorini/kotlin/hints/ifs/SafeAccessExpression.kt
@@ -0,0 +1,11 @@
+package com.igorini.kotlin.hints.ifs
+
+/* [If not null] -> Could be replaced with a safe access expression.
+ *
+ * NB: Intellij IDEA has intentions for converting between these two idioms.
+ */
+// [Before]
+fun beforeMiddleNameLength(middleName: String?) = if (middleName != null) middleName.length else null
+
+// [After]
+fun afterMiddleNameLength(middleName: String?) = middleName?.length
