8136bc11839fb08033a5c3a14d6dbf867e09f6ab
Igor Maguskin <maguskin.igor@gmail.com>
2017-07-01 22:09:31 +0100

    Added a hint to `NameClash.kt`

--- src/main/kotlin/com/igorini/kotlin/hints/imports/NameClash.kt
+++ src/main/kotlin/com/igorini/kotlin/hints/imports/NameClash.kt
@@ -1,2 +1,20 @@
 package com.igorini.kotlin.hints.imports
 
+import java.util.*
+
+// [Name clash] -> Avoid writing a fully qualified name for the class in the scenario of an import name clash
+// by disambiguating a class with a keyword `as`
+
+import java.sql.Date as SqlDate
+
+// [Before]
+fun beforeNameClash() {
+    val utilDate: Date
+    val sqlDate: java.sql.Date
+}
+
+// [After]
+fun afterNameClash() {
+    val utilDate: Date
+    val sqlDate: SqlDate
+}
