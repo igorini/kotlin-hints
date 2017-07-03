931d4b037cf9ae72a641c56643fae92ac19c1c0f
Igor Maguskin <maguskin.igor@gmail.com>
2017-07-03 23:15:49 +0100

    Add a hint to `ExtensionProperty.kt`

--- src/main/kotlin/com/igorini/kotlin/hints/extensions/ExtensionProperty.kt
+++ src/main/kotlin/com/igorini/kotlin/hints/extensions/ExtensionProperty.kt
@@ -1,2 +1,18 @@
 package com.igorini.kotlin.hints.extensions
 
+// [Extension Property] -> Commonly used expressions made on a class property can be added to a class as an extension property instead.
+
+val list = listOf(1, 2)
+
+// [Before]
+fun beforeExtensionProperty() {
+    println("Last index: ${list.size - 1}")
+}
+
+// [After]
+val <T> List<T>.lastIndex: Int
+    get() = size - 1
+
+fun afterExtensionProperty() {
+    println("Last index: ${list.lastIndex}")
+}
