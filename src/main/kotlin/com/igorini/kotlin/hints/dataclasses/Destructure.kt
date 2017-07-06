cf07eab9a07057fd77da54e7654f962cbaede334
Igor Maguskin <maguskin.igor@gmail.com>
2017-07-06 23:43:12 +0100

    Modified `Destructure.kt`

--- src/main/kotlin/com/igorini/kotlin/hints/dataclasses/Destructure.kt
+++ src/main/kotlin/com/igorini/kotlin/hints/dataclasses/Destructure.kt
@@ -1,6 +1,6 @@
 package com.igorini.kotlin.hints.dataclasses
 
-// [Destructure] -> Could destructure a data class when only it's internals are used within a scope
+// [Destructure] -> Could destructure a data class when only it's internals are used within a scope.
 
 // [Before]
 
