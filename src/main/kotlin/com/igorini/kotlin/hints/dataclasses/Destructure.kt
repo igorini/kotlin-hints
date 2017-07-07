99736a256484104bce3381982e1f632292b2c09a
Igor Maguskin <maguskin.igor@gmail.com>
2017-07-07 23:52:52 +0100

    Complete `Destructure.kt`

--- src/main/kotlin/com/igorini/kotlin/hints/dataclasses/Destructure.kt
+++ src/main/kotlin/com/igorini/kotlin/hints/dataclasses/Destructure.kt
@@ -2,6 +2,20 @@ package com.igorini.kotlin.hints.dataclasses
 
 // [Destructure] -> Could destructure a data class when only it's internals are used within a scope.
 
+data class User(val name: String = "", val age: Int = 0)
+
+val alice = User("Alice", 22)
+val ageLimit = 18
+
 // [Before]
+fun beforeDestructure() {
+    println("Name: $alice.name, Age: $alice.age")
+    if (alice.age >= ageLimit) println("$alice.name is over $ageLimit")
+}
 
 // [After]
+fun afterDestructure() {
+    val (name, age) = alice
+    println("Name: $name, Age: $age")
+    if (age >= ageLimit) println("$name is over $ageLimit")
+}
