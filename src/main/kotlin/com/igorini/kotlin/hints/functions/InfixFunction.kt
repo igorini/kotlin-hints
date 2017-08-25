21cd176bd88628fa8ceac22514845377015d3d08
Igor Maguskin <maguskin.igor@gmail.com>
2017-08-25 22:54:16 +0100

    Complete `InfixFunction.kt`

--- src/main/kotlin/com/igorini/kotlin/hints/functions/InfixFunction.kt
+++ src/main/kotlin/com/igorini/kotlin/hints/functions/InfixFunction.kt
@@ -1,7 +1,10 @@
 package com.igorini.kotlin.hints.functions
 
-// [Infix Function] ->
+// [Infix Function] -> Allows using functions in the form of `a rem b` instead of `a.rem(b)`.
 
 // [Before]
+fun beforeInfix() = 1.rem(2)
 
 // [After]
+infix fun Int.rem(num: Int) = this.rem(num)
+fun afterInfix() = 1 rem 2
